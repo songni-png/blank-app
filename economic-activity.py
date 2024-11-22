fafebf93cfda541052ffd9d84e38720fcec7f875
songni-png <soyoenjune0@gmail.com>
2024-11-22 17:10:52 +0900

    Update economic-activity.py

--- economic-activity.py
+++ economic-activity.py
@@ -1,41 +1,80 @@
-import streamlit as st
-
 # 텍스트
 st.header('전국 시군구 출생률 👼🏻')
 
-'# 이것은 제목입니다. : Magic 명령어'
-st.write('# 이것은 제목입니다. : st.write()')
-st.title('이것은 제목입니다. : st.title()')
-st.header('이것은 헤더입니다. : st.header()')
-st.subheader('이것은 서브헤더입니다. : st.subheader()')
-st.text('## 이것은 텍스트입니다. : st.text()')
-st.markdown('## 이것은 마크다운입니다. : st.markdown()')
+import streamlit as st
+import pandas as pd
+import folium
+from streamlit_folium import folium_static
+import geopandas as gpd
+import os
+import glob
+
+# 데이터 경로 설정
+data_path = os.path.abspath('전국_시군구_출생아수__합계출산율_20241119114124.csv')
+
+# CSV 데이터 불러오기
+df_korea_birthrate = pd.read_csv(data_path, header=3, encoding='utf-8')
+
+# 필요한 열만 선택
+df_korea_economics = df_korea_economics[['1102 서울 중구', '62.1']]
+
+# 데이터 정제
+df_korea_economics.columns = ['행정구', '경제활동참가율(%)']
+df_korea_economics['행정구'] = df_korea_economics['행정구'].str.replace('\d+', '', regex=True).str.strip()
+df_korea_economics['경제활동참가율(%)'] = df_korea_economics['경제활동참가율(%)'].fillna(0)
+
+st.dataframe(df_korea_economics, height=200)
+
+# GeoJSON 파일 경로 설정
+file_pattern = os.path.join('LARD_ADM_SECT_SGG_*.json')
+file_list = glob.glob(file_pattern)
+
+if not file_list:
+    raise FileNotFoundError(f"GeoJSON 파일을 찾을 수 없습니다: {file_pattern}")
+
+# GeoDataFrame 생성
+gdfs = [gpd.read_file(file) for file in file_list]
+gdf_korea_sido = gpd.GeoDataFrame(pd.concat(gdfs, ignore_index=True))
+
+# 'SGG_NM' 정제
+gdf_korea_sido['행정구'] = gdf_korea_sido['SGG_NM'].str.split().str[1:].str.join(' ')
+
+# 좌표계 변경
+korea_5179 = gdf_korea_sido.to_crs(epsg=5179)
+
+
+# 기본 지도 생성
+korea_map = folium.Map(location=[37, 126], zoom_start=7, tiles='cartodbpositron')
+
+# 제목 설정
+title = '전국 시군구 출생률'
+title_html = f'<h3 align="center" style="font-size:20px"><b>{title}</b></h3>'
+korea_map.get_root().html.add_child(folium.Element(title_html))
+
+# Choropleth map
+folium.Choropleth(
+    geo_data=gdf_korea_sido,
+    data=df_korea_economics,
+    columns=['행정구', '경제활동참가율(%)'],
+    key_on='feature.properties.행정구',
+    legend = '전국 시군구 출생률',
+    fill_color='BuPu',
+    fill_opacity=0.7,
+    line_opacity=0.3
+).add_to(korea_map)
+
+# Streamlit 설정
+st.markdown(title_html, unsafe_allow_html=True)
+
+# Folium 지도 출력
+folium_static(korea_map)
+
 
 # 사이드바
-st.header('🤖 사이드바')
-st.sidebar.write('## 사이드바 텍스트')
-st.sidebar.checkbox('체크박스 1')
-st.sidebar.checkbox('체크박스 2')
-st.sidebar.radio('라디오 버튼', ['radio 1', 'radio 2', 'radio 3'])
-st.sidebar.selectbox('셀렉트박스', ['select 1', 'select 2', 'select 3'])
+st.sidebar.write('## 연도 및 분기를 고르시오.')
+st.sidebar.selectbox('연도 및 분기', ['2023-1', '2023-2', '2023-3','2023-4','2024-1','2024-2','2024-3'])
 
-# 레이아웃: 컬럼
-st.header('🤖 컬럼 레이아웃')
-col_1, col_2, col_3 = st.columns([1,2,1]) # 컬럼 인스턴스 생성. 1:2:1 비율로 컬럼을 나눔
 
-with col_1:
-    st.write('## 1번 컬럼')
-    st.checkbox('이것은 1번 컬럼에 속한 체크박스 1')
-    st.checkbox('이것은 1번 컬럼에 속한 체크박스 2')
-
-with col_2:
-    st.write('## 2번 컬럼')
-    st.radio('2번 컬럼의 라디오 버튼', ['radio 1', 'radio 2', 'radio 3'])  # 동일한 라디오 버튼을 생성할 수 없음
-    # 사이드바에 이미 라디오 버튼이 생성되어 있기 때문에, 여기서는 라디오 버튼의 내용을 변경해야 오류가 발생하지 않음
-
-col_3.write('## 3번 컬럼')
-col_3.selectbox('3번 컬럼의 셀렉트박스', ['select 1', 'select 2', 'select 3'])
-# 사이드바에 이미 셀렉트박스가 생성되어 있기 때문에, 여기서는 셀렉트박스의 내용을 변경해야 오류가 발생하지 않음
 
 # 레이아웃: 탭
 st.header('🤖 탭 레이아웃')
