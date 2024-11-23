902fa67654489ed22e089a60cc412b78322e244d
songni-png <soyoenjune0@gmail.com>
2024-11-23 17:52:28 +0900

    Update economic-activity.py

--- economic-activity.py
+++ economic-activity.py
@@ -5,12 +5,12 @@ from streamlit_folium import folium_static
 import geopandas as gpd
 import os
 import glob
+st.cache_data.clear()
 # 텍스트
 st.header('대한민국 경제 상황과') 
 st.header('전국 행정구역 경제활동 상관관계 분석')
 
 # 사이드바
-st.cache_data.clear()
 st.sidebar.write('## 항목을 고르시오.')
 option = st.sidebar.selectbox('항목', ['경제활동참가율(%)','고용률(%)', '실업률(%)'])
 
