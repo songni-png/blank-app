c8c4a2d297e0c76c2f09a526d1ddececd1bd5f1f
songni-png <soyoenjune0@gmail.com>
2024-11-23 17:53:54 +0900

    Update economic-activity.py

--- economic-activity.py
+++ economic-activity.py
@@ -6,7 +6,7 @@ import geopandas as gpd
 import os
 import glob
 import numpy as np
-st.cache_data.clear()
+
 # 텍스트
 st.header('대한민국 경제 상황과') 
 st.header('전국 행정구역 경제활동 상관관계 분석')
@@ -18,7 +18,7 @@ option = st.sidebar.selectbox('항목', ['경제활동참가율(%)','고용률(%
 # 데이터 경로 설정
 data_path = os.path.abspath('전국_시군구_경제활동인구_총괄_20241121153501.csv')
 # CSV 데이터 불러오기
-df_korea_economics= pd.read_csv(data_path,encoding='utf-8')
+df_korea_economics = pd.read_csv(data_path, encoding='utf-8')
 
 # GeoJSON 파일 경로 설정
 file_pattern = os.path.join('LARD_ADM_SECT_SGG_*.json')
@@ -37,32 +37,24 @@ gdf_korea_sido['행정구'] = gdf_korea_sido['SGG_NM'].str.split().str[1:].str.j
 # 좌표계 변경
 korea_5179 = gdf_korea_sido.to_crs(epsg=5179)
 
-
 # 기본 지도 생성
 korea_map_1 = folium.Map(location=[37, 126], zoom_start=7, tiles='cartodbpositron')
-# 제목 설정
 title = '전국 시군구 경제활동참가율'
 title_html = f'<h3 align="center" style="font-size:20px"><b>{title}</b></h3>'
 korea_map_1.get_root().html.add_child(folium.Element(title_html))
 
-# 기본 지도 생성
 korea_map_2 = folium.Map(location=[37, 126], zoom_start=7, tiles='cartodbpositron')
-# 제목 설정
 title = '전국 시군구 고용률'
 title_html = f'<h3 align="center" style="font-size:20px"><b>{title}</b></h3>'
 korea_map_2.get_root().html.add_child(folium.Element(title_html))
 
-# 기본 지도 생성
 korea_map_3 = folium.Map(location=[37, 126], zoom_start=7, tiles='cartodbpositron')
-# 제목 설정
 title = '전국 시군구 실업률'
 title_html = f'<h3 align="center" style="font-size:20px"><b>{title}</b></h3>'
 korea_map_3.get_root().html.add_child(folium.Element(title_html))
 
-
 # 선택한 옵션에 따라 다른 코드 실행
 if option == '경제활동참가율(%)':
-    # 필요한 열만 선택
     if '경제활동참가율(%)' in df_korea_economics.columns:
         df_korea_economics = df_korea_economics[['행정구', '경제활동참가율(%)']]
     else:
@@ -73,7 +65,6 @@ if option == '경제활동참가율(%)':
     df_korea_economics['경제활동참가율(%)'] = pd.to_numeric(df_korea_economics['경제활동참가율(%)'], errors='coerce').fillna(0)
     st.dataframe(df_korea_economics, height=200)
 
-    # Choropleth map
     folium.Choropleth(
         geo_data=gdf_korea_sido,
         data=df_korea_economics,
@@ -85,24 +76,20 @@ if option == '경제활동참가율(%)':
         line_opacity=0.3
     ).add_to(korea_map_1)
 
-    # Streamlit 설정
     st.markdown(title_html, unsafe_allow_html=True)
-
-    # Folium 지도 출력
     folium_static(korea_map_1)
 
 elif option == '고용률(%)':
-    # 다른 열 선택 및 정제
     if '고용률(%)' in df_korea_economics.columns:
         df_korea_economics = df_korea_economics[['행정구', '고용률(%)']]
     else:
         df_korea_economics = df_korea_economics.iloc[:, [0, 7]]
-        df_korea_economics.columns = ['행정구', '고용률(%)']
-        df_korea_economics['행정구'] = df_korea_economics['행정구'].str.replace('\d+', '', regex=True).str.strip()
-        df_korea_economics.reset_index(drop=True, inplace=True)
-        df_korea_economics['고용률(%)'] = pd.to_numeric(df_korea_economics['고용률(%)'], errors='coerce').fillna(0)
-        st.dataframe(df_korea_economics, height=200)
-    # Choropleth map
+    df_korea_economics.columns = ['행정구', '고용률(%)']
+    df_korea_economics['행정구'] = df_korea_economics['행정구'].str.replace('\d+', '', regex=True).str.strip()
+    df_korea_economics.reset_index(drop=True, inplace=True)
+    df_korea_economics['고용률(%)'] = pd.to_numeric(df_korea_economics['고용률(%)'], errors='coerce').fillna(0)
+    st.dataframe(df_korea_economics, height=200)
+
     folium.Choropleth(
         geo_data=gdf_korea_sido,
         data=df_korea_economics,
@@ -114,25 +101,20 @@ elif option == '고용률(%)':
         line_opacity=0.3
     ).add_to(korea_map_2)
 
-    # Streamlit 설정
     st.markdown(title_html, unsafe_allow_html=True)
-
-    # Folium 지도 출력
     folium_static(korea_map_2)
 
 elif option == '실업률(%)':
-    # 또 다른 열 선택 및 정제
     if '실업률(%)' in df_korea_economics.columns:
         df_korea_economics = df_korea_economics[['행정구', '실업률(%)']]
     else:
         df_korea_economics = df_korea_economics.iloc[:, [0, 9]]
-        df_korea_economics.columns = ['행정구', '실업률(%)']
-        df_korea_economics['행정구'] = df_korea_economics['행정구'].str.replace('\d+', '', regex=True).str.strip()
-        df_korea_economics.reset_index(drop=True, inplace=True)
-        df_korea_economics['실업률(%)'] = pd.to_numeric(df_korea_economics['실업률(%)'], errors='coerce').fillna(0)
-        st.dataframe(df_korea_economics, height=200)
+    df_korea_economics.columns = ['행정구', '실업률(%)']
+    df_korea_economics['행정구'] = df_korea_economics['행정구'].str.replace('\d+', '', regex=True).str.strip()
+    df_korea_economics.reset_index(drop=True, inplace=True)
+    df_korea_economics['실업률(%)'] = pd.to_numeric(df_korea_economics['실업률(%)'], errors='coerce').fillna(0)
+    st.dataframe(df_korea_economics, height=200)
 
-    # Choropleth map
     folium.Choropleth(
         geo_data=gdf_korea_sido,
         data=df_korea_economics,
@@ -144,8 +126,5 @@ elif option == '실업률(%)':
         line_opacity=0.3
     ).add_to(korea_map_3)
 
-    # Streamlit 설정
     st.markdown(title_html, unsafe_allow_html=True)
-
-    # Folium 지도 출력
     folium_static(korea_map_3)
