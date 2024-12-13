1d62c1fe1da73b10140dbcb56ec1f6191080d050
songni-png <soyoenjune0@gmail.com>
2024-12-13 16:18:49 +0900

    Update subpage_01.py

--- pages/subpage_01.py
+++ pages/subpage_01.py
@@ -104,7 +104,7 @@ def make_heatmap(input_df_korea_economics, input_y, input_x, input_color, input_
     return heatmap
 
 # Choropleth map
-def make_choropleth(input_df, input_gj, input_column, input_color_theme):
+def make_choropleth(input_df_korea_economics, input_gj, input_column, input_color_theme):
     choropleth = px.choropleth_mapbox(input_df,
                                geojson=input_gj,
                                locations='code', 
