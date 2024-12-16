cf6533757aabc71105cb77aa61cade867b75df25
songni-png <soyoenjune0@gmail.com>
2024-12-16 16:07:18 +0900

    Update subpage_01.py

--- pages/subpage_01.py
+++ pages/subpage_01.py
@@ -23,6 +23,17 @@ df_korea_economics = pd.read_csv(data_path, header=1, encoding='utf-8')
 
 korea_geojson = json.load(open('KOREA_시도_geoJSON.json', encoding="UTF-8")) # json 파일 불러오기
 
+# CSV에서 city와 code의 매핑 생성
+csv_mapping = dict(zip(df_korea_economics['city'], df_korea_economics['code']))
+
+# GeoJSON 데이터를 GeoDataFrame으로 변환
+if isinstance(korea_geojson, dict):  # GeoJSON이 딕셔너리 형식이라면
+    korea_geojson = gpd.GeoDataFrame.from_features(korea_geojson['features'])
+
+
+# GeoJSON의 CTPRVN_CD 값을 CSV 매핑을 기반으로 업데이트
+korea_geojson['CTPRVN_CD'] = korea_geojson['CTP_KOR_NM'].map(csv_mapping).fillna(korea_geojson['CTPRVN_CD'])
+
 
 #######################
 # 데이터 전처리
@@ -38,6 +49,7 @@ df_korea_economics = df_korea_economics.melt(
                      var_name = 'property',
                      value_name = 'population',
 )
+df_korea_economics['code'] = df_korea_economics['code'].astype(str)
 
 # 연도 리스트 생성 
 years = [] 
